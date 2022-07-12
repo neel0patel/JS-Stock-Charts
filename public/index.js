2db925af780725f10f2f2f36d60fc6a6b62ad4e6
Neel Patel <neelpatel@Neels-MacBook-Pro.local>
2022-07-12 18:05:57 -0400

    Stock Charts

--- public/index.js
+++ public/index.js
@@ -1,9 +1,51 @@
-async function main() {
-
-    const timeChartCanvas = document.querySelector('#time-chart');
-    const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
-    const averagePriceChartCanvas = document.querySelector('#average-price-chart');
-
+function getColor(stock) {
+  if (stock === "GME") {
+    return "rgba(61, 161, 61, 0.7)";
+  }
+  if (stock === "MSFT") {
+    return "rgba(209, 4, 25, 0.7)";
+  }
+  if (stock === "DIS") {
+    return "rgba(18, 4, 209, 0.7)";
+  }
+  if (stock === "BNTX") {
+    return "rgba(166, 43, 158, 0.7)";
+  }
 }
 
-main()
+async function main() {
+  const timeChartCanvas = document.querySelector("#time-chart");
+  const highestPriceChartCanvas = document.querySelector(
+    "#highest-price-chart"
+  );
+  const averagePriceChartCanvas = document.querySelector(
+    "#average-price-chart"
+  );
+
+  const response = await fetch(
+    `https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=b2b0e11651294ee9a789558a1625b754`
+  );
+
+  const result = await response.json();
+
+  const { GME, MSFT, DIS, BNTX } = result;
+
+  const stocks = [GME, MSFT, DIS, BNTX];
+
+  stocks.forEach((stock) => stock.values.reverse());
+
+  // Time Chart
+  new Chart(timeChartCanvas.getContext("2d"), {
+    type: "line",
+    data: {
+      labels: stocks[0].values.map((value) => value.datetime),
+      datasets: stocks.map((stock) => ({
+        label: stock.meta.symbol,
+        backgroundColor: getColor(stock.meta.symbol),
+        borderColor: getColor(stock.meta.symbol),
+        data: stock.values.map((value) => parseFloat(value.high)),
+      })),
+    },
+  });
+  main();
+}
